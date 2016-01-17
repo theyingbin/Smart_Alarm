60e372a16ef869b6d2d164f940f4ecc5a8cdb4a3
Stan <stansjiang@gmail.com>
2016-01-16 21:12:56 -0800

    Added TimePicker into .java to pick the time. Formatted activity/content_alarm_clock.xml with new buttons and shit

--- app/src/main/java/com/example/home/smartalarm/AlarmClock.java
+++ app/src/main/java/com/example/home/smartalarm/AlarmClock.java
@@ -1,13 +1,27 @@
 package com.example.home.smartalarm;
 
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
 
 public class AlarmClock extends AppCompatActivity {
+
+    private TimePicker clock;
+    private AlarmClock alarm;
+    String displayTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +30,38 @@ public class AlarmClock extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button setTime = (Button) findViewById(R.id.setTime);
+        setTime.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+                TimePickerDialog.OnTimeSetListener timeSetListerner = new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                String am_pm = "";
+
+                                Calendar datetime = Calendar.getInstance();
+                                datetime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                                datetime.set(Calendar.MINUTE, minute);
+
+                                if (datetime.get(Calendar.AM_PM) == Calendar.AM)
+                                    am_pm = "AM";
+                                else if (datetime.get(Calendar.AM_PM) == Calendar.PM)
+                                    am_pm = "PM";
+
+                                String strHrsToShow = (datetime.get(Calendar.HOUR) == 0) ? "12" : datetime.get(Calendar.HOUR) + "";
+
+                                TextView txtView = (TextView)findViewById(R.id.txtView);
+                                txtView.setText( strHrsToShow+":"+datetime.get(Calendar.MINUTE)+" "+am_pm );
+
+//                                public void onClick(View v) {
+//                                    txtView.setText(displayTime);
+//                                }
+                            }
+                        };
+
+
             }
         });
     }
-
 }
